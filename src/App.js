16f395c70ab7bb5129a1d22e86c068ed08f4bf96
dima <zhuikovda.gmail.com>
2022-11-22 16:18:44 +0300

    lesson 1-7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return  <div>
+    <Header />
+    <Tehnologies />  
+    </div>;
+}
+
+
+const Header = () => {
+  return  <div>
+      <a href="#s">Home</a>
+      <a href="#s">New Feed</a>
+      <a href="#s">Messages</a>
+    </div>;
+}
+
+const Tehnologies = () => {
+  return <div className="App">
+  <ul>
+    <li>css</li>
+    <li>html</li>
+    <li>js</li>
+    <li>dima</li>
+  </ul>
+</div>
 }
 
 export default App;
