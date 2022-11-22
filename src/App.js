ccb61a0ab289502a0a86d5fb29bb083f71171b67
dima <zhuikovda.gmail.com>
2022-11-22 18:31:45 +0300

    lesson 8-9

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './header'; 
+import Tehnologies from './technologies';
+
 
 const App = () => {
   return  <div>
     <Header />
     <Tehnologies />  
     </div>;
-}
-
-
-const Header = () => {
-  return  <div>
-      <a href="#s">Home</a>
-      <a href="#s">New Feed</a>
-      <a href="#s">Messages</a>
-    </div>;
-}
-
-const Tehnologies = () => {
-  return <div className="App">
-  <ul>
-    <li>css</li>
-    <li>html</li>
-    <li>js</li>
-    <li>dima</li>
-  </ul>
-</div>
 }
 
 export default App;
---
+++ src/technologies.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Tehnologies = () => {
+    return <div className="App">
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  }
+
+  export default Tehnologies;
