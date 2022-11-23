da1dcb337b9e5b3a39bf48a2f636cee7b1857df0
dima <zhuikovda.gmail.com>
2022-11-23 23:47:24 +0300

    lesson 10

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import Header from './header'; 
-import Tehnologies from './technologies';
+import icons8Img from "./img/icons8.png"
+import greenMan from "./img/green_man.jpg"
+
 
 
 const App = () => {
-  return  <div>
-    <Header />
-    <Tehnologies />  
+  return  <div className='app_wrapper'>
+    <header className='header'>
+       <img className='header__logo' src={icons8Img} alt='' />      
+    </header>
+    <nav className="nav">
+      <ul >
+        <li><a>Profile</a></li>
+        <li><a>Massages</a></li>
+        <li><a>News</a></li>
+        <li><a>Music</a></li>
+        <li><a>Settings</a></li>                
+      </ul>
+    </nav>
+    <div className='content'>
+      <img src={greenMan}></img>
+    </div>
+
     </div>;
 }
 
